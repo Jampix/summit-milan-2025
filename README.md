718ffdef5ec676dc8f4c00d8f6f2d7c543cb8dab
Pasquale Mazzei <pasquale.mazzei@neosperience.com>
2025-05-21 08:56:00 +0200

    init app

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# summit-milan-2025
-Demo for Community Lounge Milan 18 June 2025
+# Welcome to your CDK TypeScript project
+
+This is a blank project for CDK development with TypeScript.
+
+The `cdk.json` file tells the CDK Toolkit how to execute your app.
+
+## Useful commands
+
+* `npm run build`   compile typescript to js
+* `npm run watch`   watch for changes and compile
+* `npm run test`    perform the jest unit tests
+* `npx cdk deploy`  deploy this stack to your default AWS account/region
+* `npx cdk diff`    compare deployed stack with current state
+* `npx cdk synth`   emits the synthesized CloudFormation template
---
+++ test/summit-milan-2025.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SummitMilan2025 from '../lib/summit-milan-2025-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/summit-milan-2025-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SummitMilan2025.SummitMilan2025Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
